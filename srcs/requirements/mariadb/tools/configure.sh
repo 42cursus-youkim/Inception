9891e1b4e66d67616952a034f0717a1289484354
scarf <greenscarf005@gmail.com>
2022-07-02 17:50:09 +0900

    refactor: 함수로 더 빼고 herestring으로 수정

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -6,7 +6,7 @@ source $(dirname "$0")/utils.sh
 init_database() {
   local DATADIR=${1:-'/var/lib/mysql'}
 
-  chown -R mysql:mysql $DATADIR
+  mkowndir $DATADIR mysql
 
   # init database
   mysql_install_db \
@@ -21,20 +21,18 @@ init_database() {
 
 init_query() {
   # https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
-  # run init.sql
   local file=${1:-'/tmp/tools/init.sql'}
   local query=$(subst $file)
   log created query: $query
-  /usr/bin/mysqld --user=mysql --bootstrap <$query
+  /usr/bin/mysqld --user=mysql --bootstrap <<<$query
+
   log bootstrap finished
 }
 
 # Main
 MYSQLD="/run/mysqld"
 ifnotdir $MYSQLD && {
-  mkdir -p $MYSQLD
-  chown -R mysql:mysql $MYSQLD
-  log created $MYSQLD
+  mkowndir $MYSQLD mysql
 }
 
 ifnotdir /var/lib/mysql/mysql && {
@@ -46,4 +44,5 @@ ifnotdir /var/lib/mysql/mysql && {
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
+log running /usr/bin/mysqld
 exec /usr/bin/mysqld --user=mysql --console
--- srcs/requirements/mariadb/tools/utils.sh
+++ srcs/requirements/mariadb/tools/utils.sh
@@ -22,6 +22,15 @@ log() {
   echo "[-]" $@
 }
 
+mkowndir() {
+  local dir=$1
+  local user=$2
+  local group=${3:-$user}
+  mkdir -p $dir
+  chown -R $user:$group $dir
+  log created $dir and gave permissions to $user:$group
+}
+
 # 전역 변수 목록으로 문자열 치환
 subst() {
   text=$(<$1)
