90f55e8c8a5678287bdc35edd8843c805e55088c
scarf <greenscarf005@gmail.com>
2022-07-06 11:15:55 +0900

    fix: 일?단은 고침

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,18 +1,25 @@
 #!/bin/ash
 
-wait_service_port() {
-  local max_retry=$1
-  local target="$2"
-  shift 2
-  until [ $max_retry -eq 0 ] || nc -zv -w 1 "$@"; do
-    echo "[-] Wait for" $target "-" $max_retry
-    let "max_retry=max_retry-1"
-    sleep 5
-  done
-  if [ $max_retry -eq 0 ]; then
-    exit 1
-  fi
-}
+# wait_service_port() {
+#   local max_retry=$1
+#   local target="$2"
+#   shift 2
+#   until [ $max_retry -eq 0 ] || nc -zv -w 1 "$@"; do
+#     echo "[-] Wait for" $target "-" $max_retry
+#     let "max_retry=max_retry-1"
+#     sleep 5
+#   done
+#   if [ $max_retry -eq 0 ]; then
+#     exit 1
+#   fi
+# }
+
+# TODO: healthcheck로 바꾸기
+while ! mariadb -h mariadb -u $DB_USER -p$DB_PASSWORD $DB_NAME; do
+  echo "waiting..."
+  sleep 3
+done
+echo "[-] connection success."
 
 if [ ! -d '/var/www/wordpress' ]; then
   wp core download --path=/var/www/wordpress
@@ -30,8 +37,8 @@ if [ ! -d '/var/www/wordpress' ]; then
     --admin_password="$WP_ADMIN_PASSWORD" \
     --admin_email="$WP_ADMIN_EMAIL"
 
-  echo "attempting to connect..."
-  wait_service_port 30 "mariadb" "mariadb" 3306
+  # echo "attempting to connect..."
+  # wait_service_port 30 "mariadb" "mariadb" 3306
 
   # wp plugin install \
   # --path=/var/www/wordpress redis-cache --activate
@@ -40,8 +47,10 @@ if [ ! -d '/var/www/wordpress' ]; then
   # curl -L -o /var/www/wordpress/adminer.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php
   # sed -i "/Add any custom values between/ a define( 'WP_REDIS_HOST', 'redis' );" /var/www/wordpress/wp-config.php
 else
-  wait_service_port 30 "mariadb" "mariadb" 3306
+  echo "Wordpress already installed"
+  # wait_service_port 30 "mariadb" "mariadb" 3306
   # wait_service_port 30 "redis" "redis" 6379
 fi
 
+echo attempting to run php-fpm...
 exec php-fpm8 -F
