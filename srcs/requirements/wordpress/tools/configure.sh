c9bb2c279c8cd4da1fa89d905563fbcc0efbb363
scarf <greenscarf005@gmail.com>
2022-07-06 09:30:06 +0900

    fix: 버전  관련

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -14,32 +14,34 @@ wait_service_port() {
   fi
 }
 
-if [ ! -d '/srv/www/wordpress' ]; then
-  wp core download --path=/srv/www/wordpress
-  wait_service_port 30 "mariadb" "mariadb" 3306
+if [ ! -d '/var/www/wordpress' ]; then
+  wp core download --path=/var/www/wordpress
   wp core config \
-    --path=/srv/www/wordpress \
+    --path=/var/www/wordpress \
     --dbhost=mariadb \
     --dbname="$DB_NAME" \
     --dbuser="$DB_USER" \
     --dbpass="$DB_PASSWORD"
   wp core install \
-    --path=/srv/www/wordpress \
+    --path=/var/www/wordpress \
     --url="$WP_URL" \
     --title="$WP_TITLE" \
     --admin_name="$WP_ADMIN_NAME" \
     --admin_password="$WP_ADMIN_PASSWORD" \
     --admin_email="$WP_ADMIN_EMAIL"
 
+  echo "attempting to connect..."
+  wait_service_port 30 "mariadb" "mariadb" 3306
+
   # wp plugin install \
-    # --path=/srv/www/wordpress redis-cache --activate
+  # --path=/var/www/wordpress redis-cache --activate
   # wait_service_port 30 "redis" "redis" 6379
-  # wp redis enable --path=/srv/www/wordpress
-  # curl -L -o /srv/www/wordpress/adminer.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php
-  # sed -i "/Add any custom values between/ a define( 'WP_REDIS_HOST', 'redis' );" /srv/www/wordpress/wp-config.php
+  # wp redis enable --path=/var/www/wordpress
+  # curl -L -o /var/www/wordpress/adminer.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php
+  # sed -i "/Add any custom values between/ a define( 'WP_REDIS_HOST', 'redis' );" /var/www/wordpress/wp-config.php
 else
   wait_service_port 30 "mariadb" "mariadb" 3306
   # wait_service_port 30 "redis" "redis" 6379
 fi
 
-exec php-fpm7 -F
+exec php-fpm8 -F
