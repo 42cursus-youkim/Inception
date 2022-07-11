6031015317388adaafdd1f2f0de64d735ce6a2c9
scarf <greenscarf005@gmail.com>
2022-07-11 18:43:57 +0900

    chore: 스크립트 설명 추가

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+echo Stoping containers
 docker stop $(docker ps -qa)
+
+echo Removing containers
 docker rm $(docker ps -qa)
+
+echo Removing images
 docker rmi $(docker images -qa)
+
+echo Removing volumes
 docker volume rm $(docker volume ls -q)
+
+echo Removing networks
 docker network rm $(docker network ls -q) 2>/dev/null
